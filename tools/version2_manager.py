5e96e5521cf4f060a0c423d4f50704b616a9c735
feede333 <your.email@example.com>
2025-04-12 22:22:23 -0300

    Updated versions for money-talks-details.html

--- tools/version2_manager.py
+++ tools/version2_manager.py
@@ -719,7 +719,8 @@ class VersionManager:
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>{program_name} {year} - Versiones</title>
-                <link rel="stylesheet" href="../../csscomun.css">
+                <link rel="stylesheet" href="../../css/csscomun.css">
+
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
                 <script src="../../js/torrentTrackerversiones.js"></script>
             </head>
